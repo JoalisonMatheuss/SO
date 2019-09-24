9c2ca42c8ea4639fe6c1c6ac8ba55f907af9ae99
JoalisonMatheuss <44174090+JoalisonMatheuss@users.noreply.github.com>
2019-09-24 14:33:18 -0300

    Acho que está certo

--- Questao-1.py
+++ Questao-1.py
@@ -13,31 +13,44 @@ def child():
 	elif opcao == 3:
 		comando3 = ["/snap/bin/gnome-calculator"]
 		os.execve(comando3[0], comando3, os.environ)
-	else:
+	elif opcao == 4:
 		comando4 = ["/usr/bin/nautilus"]
 		os.execve(comando4[0], comando4, os.environ)
 		
 
 def parent(newpid):
-	pids = (os.getpid(), newpid)
-	print("Processo pai: %d, filho: %d \n"%pids)
-	ret = os.waitpid(newpid, 0)
-	print("Filho terminou com status: ", ret)
+	if (parar == 1):
+		pids = (os.getpid(), newpid)
+		print("Processo pai: %d, filho: %d \n"%pids)
+		ret = os.waitpid(newpid, 0)
+		print("Filho terminou com status: ", ret)
+	else:
+		pids = (os.getpid(), newpid)
+		print("Processo pai: %d, filho: %d \n"%pids)
 
 while True:
-
-
+	
 	os.system('clear')
-	print("------------------------------ Menu -----------------------------")
+	print("============== ESPERAR/ OU NÃO ESPERAR =================")
+	print("0 --> Não Esperar")
+	print("1 --> Esperar")
+	print("========================================================")
+	
+	parar = int(input("Digite sua escolha: "))                     
+	
+	os.system('clear')
+	print("------------------------------ MY SHELL -----------------------------")
 	print("|			1 - firefox				|")
 	print("|			2 - gedit				|")
 	print("|			3 - gnome-calculator			|")
 	print("|			4 - nautilus				|")
-	print("-----------------------------------------------------------------\n")
+	print("-------------------------------------------------------------------\n")
 
 
 	opcao = int(input("Digite o que você quer que abra: "))
+
 	
+
 
 	newpid = os.fork()
 	if newpid == 0:
